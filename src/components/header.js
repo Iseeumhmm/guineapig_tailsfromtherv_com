54ecf68032d959059f66bdda5d055ce73701a765
Iseeumhmm <rickheffren@Ricks-MacBook-Pro.local>
2019-07-03 11:16:01 -0400

    fixing for netify

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,9 @@
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
-import Form from 'react-bootstrap/Form'
+// import NavDropdown from 'react-bootstrap/NavDropdown'
+// import Form from 'react-bootstrap/Form'
 // import FormControl from 'react-bootstrap/FormControl'
 // import Button from 'react-bootstrap/Button';
 
