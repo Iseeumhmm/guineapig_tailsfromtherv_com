9f9d590b4ea4ed0cdba5ebb152e2987f200e0492
Iseeumhmm <rickheffren@Ricks-MacBook-Pro.local>
2019-07-06 20:00:37 -0400

    parallax solved

--- src/components/happening-now.js
+++ src/components/happening-now.js
@@ -6,7 +6,7 @@ function happeningNow( props ) {
     return (
         <div className="happening_now-container">
             <p style={{backgroundColor: props.highlightColour}} className="happening_now-heading box-shadow border-radius">2019 Maritime Tour</p>
-            <p className="happening_now-text">Blizzard is going to Canada's east coast</p>
+            {/* <p className="happening_now-text">Blizzard is going to Canada's east coast</p> */}
         </div>
         
 )};
--- src/pages/index.js
+++ src/pages/index.js
@@ -77,30 +77,32 @@ const Blog = () => (
         window.addEventListener('scroll', () => {
           var scrolled = window.pageYOffset;
           const background = document.querySelector('.background');
-          background.style.top = -(scrolled * -0.2) + 'px';
+          background.style.top = -(scrolled * -0.65) + 'px';
         });
       }, []);
       return ( 
       <Layout>
         <SEO title="Home" />
-        <HappeningNow 
+        <BackgroundImage Tag="div"
+          className="parallax-image background"
+          fluid={pages.acf.main_banner_image.localFile.childImageSharp.fluid}
+          backgroundColor={`#040e18`}>
+          <HappeningNow 
+                className="happening_now-parallax"
                 key={1} 
-                highlightColour={pages.acf.highlight_color}/>      
-            <BackgroundImage Tag="div"
-              className="background parallax-image"
-              fluid={pages.acf.main_banner_image.localFile.childImageSharp.fluid}
-              backgroundColor={`#040e18`}/>
-              <p className="banner-text">Coming this July</p>
-              <Carousel key={3} images={posts.edges}/>
-              <LatestPost 
-                key={4} 
-                title={post.title}
-                slug={post.slug}
-                location={post.acf.location}
-                what={post.acf.excerpt}
-                image={post.acf.image.localFile.childImageSharp}
-                highlight={post.acf.highlight_color}/>
-              <PostList key={5} data={posts.edges}/>
+                highlightColour={pages.acf.highlight_color}/>  
+        </BackgroundImage>
+          <p className="banner-text">Coming this July</p>
+          <Carousel key={3} images={posts.edges}/>
+          <LatestPost 
+            key={4} 
+            title={post.title}
+            slug={post.slug}
+            location={post.acf.location}
+            what={post.acf.excerpt}
+            image={post.acf.image.localFile.childImageSharp}
+            highlight={post.acf.highlight_color}/>
+          <PostList key={5} data={posts.edges}/>
       </Layout>
 )}}
 />
