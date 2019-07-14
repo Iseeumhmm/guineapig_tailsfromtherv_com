66b2ee864e68085c9e6907c8b5e5a9d75685be05
Iseeumhmm <rickheffren@Ricks-MacBook-Pro.local>
2019-07-14 11:05:27 -0400

    image problem

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ const Blog = () => (
                         ...GatsbyImageSharpFixed_withWebp
                         }
                       fluid {
-                        ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                        ...GatsbyImageSharpFluid
                       }
                     }
                   }
